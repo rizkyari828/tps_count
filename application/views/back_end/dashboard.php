db6afee14d974c8a99bd40ae9580093589354be8
Rizky Ari Pratama <BSI50164@bsi.co.id>
2024-02-13 16:48:21 +0700

    add clock

--- application/views/back_end/dashboard.php
+++ application/views/back_end/dashboard.php
@@ -1,3 +1,25 @@
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Oswald:wght@300;400&display=swap");
+
+#clock {
+  max-width: 600px;
+}
+
+/* for smaller screens below 700px */
+@media only screen and (max-width: 700px) {
+  body {
+    font-size: 1.8rem;
+  }
+}
+
+/*for smaller screens below 300px*/
+@media only screen and (max-width: 300px) {
+  body {
+    font-size: 1.6rem;
+  }
+}
+</style>
+
 <!-- DataTables -->
 <div class="content-wrapper">
     <!-- Content Header (Page header) -->
@@ -9,7 +31,9 @@
     </section>
     
     <section class="content">
-     
+    <div id="clock">
+      <h1 id="date-time"></h1>
+    </div>
           <div class="box box-danger"  style="padding: 20px;">
             <div class="card">
               <div class="card-body">
@@ -198,3 +222,63 @@
               
     </section>
 </div>
+
+<script>
+window.addEventListener("load", () => {
+  clock();
+  function clock() {
+    const today = new Date();
+
+    // get time components
+    const hours = today.getHours();
+    const minutes = today.getMinutes();
+    const seconds = today.getSeconds();
+
+    //add '0' to hour, minute & second when they are less 10
+    const hour = hours < 10 ? "0" + hours : hours;
+    const minute = minutes < 10 ? "0" + minutes : minutes;
+    const second = seconds < 10 ? "0" + seconds : seconds;
+
+    //make clock a 12-hour time clock
+    const hourTime = hour > 12 ? hour - 12 : hour;
+
+    // if (hour === 0) {
+    //   hour = 12;
+    // }
+    //assigning 'am' or 'pm' to indicate time of the day
+    const ampm = hour < 12 ? "AM" : "PM";
+
+    // get date components
+    const month = today.getMonth();
+    const year = today.getFullYear();
+    const day = today.getDate();
+
+    //declaring a list of all months in  a year
+    const monthList = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+
+    //get current date and time
+    const date = monthList[month] + " " + day + ", " + year;
+    const time = hourTime + ":" + minute + ":" + second + ampm;
+
+    //combine current date and time
+    const dateTime = date + " - " + time;
+
+    //print current date and time to the DOM
+    document.getElementById("date-time").innerHTML = dateTime;
+    setTimeout(clock, 1000);
+  }
+});
+</script>
